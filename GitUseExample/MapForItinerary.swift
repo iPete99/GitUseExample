9167bc43e7c30878ca2b2b6ebbf492712b379a60
PeteAndFuzzy <szepanski@gmail.com>
2019-04-20 21:13:26 +0200

    Adding a new map_feature feature

---
+++ GitUseExample/MapForItinerary.swift
@@ -0,0 +1,13 @@
+//
+//  MapForItinerary.swift
+//  GitUseExample
+//
+//  Created by Peter and Fuzzy on 20/4/19.
+//  Copyright © 2019 Peter and Fuzzy. All rights reserved.
+//
+
+import UIKit
+
+class MapForItinerary: NSObject {
+    
+}
